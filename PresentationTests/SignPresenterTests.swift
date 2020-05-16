f9994cceb2ffaedceb3bc1a4f5226c6645304db3
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-16 17:41:45 -0300

    Criação do sut movido para helper

--- PresentationTests/SignPresenterTests.swift
+++ PresentationTests/SignPresenterTests.swift
@@ -45,16 +45,14 @@ struct SignViewModel {
 
 class SignPresenterTests: XCTestCase {
     func test_signUp_should_show_error_message_if_cpf_is_not_provider() {
-        let alertViewSpy = AlertViewSpy()
-        let sut = SignPresenter(alertView: alertViewSpy)
+        let (sut, alertViewSpy) = makeSut()
         let signUpViewModel = SignViewModel(name: "any_name", dataNascimento: "any_nascimento", anoConclusaoEnsinoMedio: 2020, email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
         sut.signUp(viewModel: signUpViewModel)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "O campo cpf é obrigatorio"))
     }
     
     func test_signUp_should_show_error_message_if_name_is_not_provider() {
-        let alertViewSpy = AlertViewSpy()
-        let sut = SignPresenter(alertView: alertViewSpy)
+        let (sut, alertViewSpy) = makeSut()
         let signUpViewModel = SignViewModel(cpf: "any_cpf", dataNascimento: "any_nascimento", anoConclusaoEnsinoMedio: 2020, email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
         sut.signUp(viewModel: signUpViewModel)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "O campo nome é obrigatorio"))
@@ -62,6 +60,12 @@ class SignPresenterTests: XCTestCase {
 }
 
 extension SignPresenterTests {
+    func makeSut() -> (sut: SignPresenter, alertViewSpy: AlertViewSpy) {
+        let alertViewSpy = AlertViewSpy()
+        let sut = SignPresenter(alertView: alertViewSpy)
+        return (sut, alertViewSpy)
+    }
+    
     class AlertViewSpy: AlertView {
         var viewModel: AlertViewModel?
         

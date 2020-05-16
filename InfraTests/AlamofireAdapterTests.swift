9d268d6810d7da797f1d3e3aeb4bb79c93f6d89f
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-15 22:07:49 -0300

    Adicionado Infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,16 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Julio Figueiredo on 15/05/20.
+//  Copyright © 2020 Julio Figueiredo. All rights reserved.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+    func test_() {
+        
+    }
+
+}

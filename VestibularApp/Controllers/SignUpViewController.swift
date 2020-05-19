5cdc7fa81435bf0b266a05b32005517734907208
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-19 16:08:02 -0300

    Adicionado telas de signUp e addAccount

---
+++ VestibularApp/Controllers/SignUpViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SignUpViewController.swift
+//  VestibularApp
+//
+//  Created by Julio Figueiredo on 18/05/20.
+//  Copyright © 2020 Julio Figueiredo. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    @IBOutlet weak var tfCpf: UITextField!
+    @IBOutlet weak var tfSenha: UITextField!
+    @IBOutlet weak var btEntrar: UIButton!
+    @IBOutlet weak var btCadastrar: UIButton!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    @IBOutlet weak var vwLoading: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func signUp(_ sender: Any) {
+        
+    }
+}

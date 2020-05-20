89d9c46a21c98da1a4f7056e8a8d1a3bac699b31
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-20 20:22:09 -0300

    Adicionado Validation target

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Julio Figueiredo on 20/05/20.
+//  Copyright © 2020 Julio Figueiredo. All rights reserved.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+    func test_() {
+        
+    }
+}

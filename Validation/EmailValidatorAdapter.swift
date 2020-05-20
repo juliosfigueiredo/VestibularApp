f0da672842a28ed5de72d9d1c758c3bc0aa07947
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-20 20:41:36 -0300

    EmailValidatorAdapter movida para um arquivo

---
+++ Validation/EmailValidatorAdapter.swift
@@ -0,0 +1,22 @@
+//
+//  EmailValidatorAdapter.swift
+//  Validation
+//
+//  Created by Julio Figueiredo on 20/05/20.
+//  Copyright © 2020 Julio Figueiredo. All rights reserved.
+//
+
+import Foundation
+import Presentation
+
+public final class EmailValidatorAdapter: EmailValidator {
+    private let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    public init() {}
+    
+    public func isValid(email: String) -> Bool {
+        let range = NSRange(location: 0, length: email.utf16.count)
+        let regex = try! NSRegularExpression(pattern: pattern)
+        return regex.firstMatch(in: email, options: [], range: range) != nil
+    }
+}

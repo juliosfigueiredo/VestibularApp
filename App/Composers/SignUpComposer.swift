fbcc84e692a8e820f4318367c507768b89af17ac
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-21 17:22:07 -0300

    ControllerFactory removido

--- App/Composers/SignUpComposer.swift
+++ App/Composers/SignUpComposer.swift
@@ -7,10 +7,16 @@
 //
 
 import Foundation
+import Presentation
+import Validation
 import Domain
 
 public final class SignUpComposer {
     public static func composeControllerWith(addAccount: AddAccount) -> SignUpViewController {
-        return ControllerFactory.makeSignUp(addAccount: addAccount)
+        let controller = SignUpViewController.instantiate()
+        let emailValidatorAdapter = EmailValidatorAdapter()
+        let presenter = SignPresenter(alertView: WeakVarProxy(controller), emailValidator: emailValidatorAdapter, addAccount: addAccount, loadingView: WeakVarProxy(controller))
+        controller.signUp = presenter.signUp
+        return controller
     }
 }

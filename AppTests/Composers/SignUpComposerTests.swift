19b0d7f11f917e75d21592c333fe74a2aea28075
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-21 19:18:38 -0300

    Criação do Sut movida para um método factory

--- AppTests/Composers/SignUpComposerTests.swift
+++ AppTests/Composers/SignUpComposerTests.swift
@@ -10,12 +10,25 @@ import XCTest
 @testable import App
 
 class SignUpComposerTests: XCTestCase {
-    func test_ui_presentation_integration() {
-        debugPrint("====================")
-        debugPrint(Environment.variable(.apiBaseUrl))
-        debugPrint("====================")
-        let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
-        checkMemoryLeak(for: sut)
+    func test_background_request_should_complete_on_main_thread() {
+        //debugPrint("====================")
+        //debugPrint(Environment.variable(.apiBaseUrl))
+        //debugPrint("====================")
+        let (sut, _) = makeSut()
+        sut.loadViewIfNeeded()
+        DispatchQueue.global().async {
+            
+        }
+    }
+}
+
+extension SignUpComposerTests {
+    func makeSut(file: StaticString = #file, line: UInt = #line) -> (sut: SignUpViewController, addAccountSpy: AddAccountSpy) {
+        let addAccountSpy = AddAccountSpy()
+        let sut = SignUpComposer.composeControllerWith(addAccount: addAccountSpy)
+        checkMemoryLeak(for: sut, file: file, line: line)
+        checkMemoryLeak(for: addAccountSpy, file: file, line: line)
+        return (sut, addAccountSpy)
     }
 }
 

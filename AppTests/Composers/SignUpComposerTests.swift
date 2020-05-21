6a2b0c65840349e7601b3f5f6dcbbfa54e4e1300
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-21 20:08:38 -0300

    Threading login movida para uma classe decorator

--- AppTests/Composers/SignUpComposerTests.swift
+++ AppTests/Composers/SignUpComposerTests.swift
@@ -14,18 +14,22 @@ class SignUpComposerTests: XCTestCase {
         //debugPrint("====================")
         //debugPrint(Environment.variable(.apiBaseUrl))
         //debugPrint("====================")
-        let (sut, _) = makeSut()
+        let (sut, addAccountSpy) = makeSut()
         sut.loadViewIfNeeded()
+        sut.signUp?(makeSignUpViewModel())
+        let exp = expectation(description: "waiting")
         DispatchQueue.global().async {
-            
+            addAccountSpy.completeWithError(.unexpected)
+            exp.fulfill()
         }
+        wait(for: [exp], timeout: 1)
     }
 }
 
 extension SignUpComposerTests {
     func makeSut(file: StaticString = #file, line: UInt = #line) -> (sut: SignUpViewController, addAccountSpy: AddAccountSpy) {
         let addAccountSpy = AddAccountSpy()
-        let sut = SignUpComposer.composeControllerWith(addAccount: addAccountSpy)
+        let sut = SignUpComposer.composeControllerWith(addAccount: MainQueueDispatchDecorator(addAccountSpy))
         checkMemoryLeak(for: sut, file: file, line: line)
         checkMemoryLeak(for: addAccountSpy, file: file, line: line)
         return (sut, addAccountSpy)

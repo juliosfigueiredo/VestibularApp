93939e0e43e6f4ff080d3cd46164ce20bcbf0d9d
Julio Figueiredo <juliosfigueiredo@gmail.com>
2020-05-21 17:17:29 -0300

    WeakVarProxy movido para um arquivo

---
+++ App/Proxies/WeakVarProxy.swift
@@ -0,0 +1,30 @@
+//
+//  WeakVarProxy.swift
+//  App
+//
+//  Created by Julio Figueiredo on 21/05/20.
+//  Copyright © 2020 Julio Figueiredo. All rights reserved.
+//
+
+import Foundation
+import Presentation
+
+final class WeakVarProxy<T: AnyObject> {
+    private weak var instance: T?
+    
+    init(_ instance: T) {
+        self.instance = instance
+    }
+}
+
+extension WeakVarProxy: AlertView where T: AlertView {
+    func showMessage(viewModel: AlertViewModel) {
+        instance?.showMessage(viewModel: viewModel)
+    }
+}
+
+extension WeakVarProxy: LoadingView where T: LoadingView {
+    func display(viewModel: LoadingViewModel) {
+        instance?.display(viewModel: viewModel)
+    }
+}
